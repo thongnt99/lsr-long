ff0a5247f3d2e6d7f163aa5fcd773a7d878fb75a
Thong Nguyen <thongnguyenthac@gmail.com>
2023-04-17 19:33:02 +0200

    update scripts

--- scripts/preprare_msmarco_doc.sh
+++ scripts/preprare_msmarco_doc.sh
@@ -2,13 +2,14 @@ echo "Download msmarco documents"
 python lsr/preprocess/prepare_msmarco_doc.py
 # Split the collection into 60 splits 
 echo "Split the collection into 60 partitions"
-mkdir data/msmarco_doc/splits
+mkdir -p data/msmarco_doc/splits
 split --numeric-suffixes=1 --number=l/60  dât/msmarco_doc/collection.tsv data/msmarco_doc/splits/part
 # Split long documents into passages 
 echo "Split long documents into passages"
+mkdir -p data/msmarco_doc/splits_psg
 for f in data/msmarco_doc/splits/*;
 do
 echo $f;
-python lsr/long_documents/split_long_documnets.py $f & # remove the & if you want to run sequentially
+python lsr/long_documents/split_long_documents.py $f & # remove the & if you want to run sequentially
 done
 echo "Running inference on document passages"
--- scripts/preprare_robust04.sh
+++ scripts/preprare_robust04.sh
@@ -2,14 +2,14 @@ echo "Download msmarco documents"
 python lsr/preprocess/prepare_robust04.py
 # Split the collection into 60 splits 
 echo "Split the collection into 60 partitions"
-mkdir data/trec-robust04/splits
+mkdir -p data/trec-robust04/splits
 split --numeric-suffixes=1 --number=l/60 data/trec-robust04/collection.tsv data/trec-robust04/splits/part
 # Split long documents into passages 
 echo "Split long documents into passages"
-mkdir data/trec-robust04/splits_psg
+mkdir -p data/trec-robust04/splits_psg
 for f in data/trec-robust04/splits/*;
 do
 echo $f;
-python lsr/long_documents/split_long_documnets.py $f & # remove the & if you want to run sequentially
+python lsr/long_documents/split_long_documents.py $f & # remove the & if you want to run sequentially
 done
 echo "Running inference on document passages"
