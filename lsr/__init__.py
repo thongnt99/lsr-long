7dee581c8976901c6423673c9b5501d3d23affc5
Thong Nguyen <thongnguyenthac@gmail.com>
2023-04-17 21:31:16 +0200

    not downloading checkpoint at import

--- lsr/__init__.py
+++
@@ -1,6 +0,0 @@
-from huggingface_hub import snapshot_download
-from pathlib import Path
-repo_path = "qmlp_dmlm_msmarco_distil_kl_l1_0.0001"
-if not Path(repo_path).is_dir():
-    snapshot_download(repo_id="lsr42/qmlp_dmlm_msmarco_distil_kl_l1_0.0001",
-                      local_dir="qmlp_dmlm_msmarco_distil_kl_l1_0.0001")
