c89c2e9d7ec3b0bbf155a99ecf790fc57a5aba29
Thong Nguyen <thongnguyenthac@gmail.com>
2023-04-18 01:11:24 +0200

    datasets

--- lsr/datasets/__init__.py
+++ lsr/datasets/__init__.py
@@ -1,6 +1,6 @@
-from .triplet_dataset import TripletIdsDataset, TripletTextDataset
-from .hard_negatives import TripletDistilDataset, TripletIDDistilDataset
-from .tokenized_multiple_negatives import GroupedMarcoTrainDataset
-from .term_recall_dataset import TermRecallDataset
-from .rerank_data import RerankData
-from .rerank_collator import RerankCollator
+# from .triplet_dataset import TripletIdsDataset, TripletTextDataset
+# from .hard_negatives import TripletDistilDataset, TripletIDDistilDataset
+# from .tokenized_multiple_negatives import GroupedMarcoTrainDataset
+# from .term_recall_dataset import TermRecallDataset
+# from .rerank_data import RerankData
+# from .rerank_collator import RerankCollator
