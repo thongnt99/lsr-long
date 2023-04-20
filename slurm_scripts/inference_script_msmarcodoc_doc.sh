6421747699190f2e23832104eb49ba226a530550
Thong Nguyen <thongnguyenthac@gmail.com>
2023-04-20 22:10:53 +0200

    increase request time

--- slurm_scripts/inference_script_msmarcodoc_doc.sh
+++ slurm_scripts/inference_script_msmarcodoc_doc.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres gpu:1
 #SBATCH -c 5
 #SBATCH --mem-per-cpu 15000
-#SBATCH -t 0-00:30:00
+#SBATCH -t 0-01:00:00
 #SBATCH -o slurm_scripts/logs/inference_msmarco_doc.out
 #SBATCH -e slurm_scripts/logs/inference_msmarco_doc.err
 #SBATCH -a 1-60%60
@@ -12,9 +12,7 @@
 eval "$(conda shell.bash hook)"
 # Activate a conda environment:
 conda activate lsr 
-
 trap "trap ' ' TERM INT; kill -TERM 0; wait" TERM INT
-
 input_path=data/msmarco_doc/splits_psg/part$(printf "%02d" $SLURM_ARRAY_TASK_ID)
 output_path=data/msmarco_doc/vectors/part$(printf "%02d" $SLURM_ARRAY_TASK_ID)
 batch_size=256
