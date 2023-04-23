bd35a66dc48ab898d7e9e3df163456e57d1b9de4
Thong Nguyen <thongnguyenthac@gmail.com>
2023-04-23 03:50:38 +0200

    update code

--- lsr/preprocess/prepare_bm25.py
+++ lsr/preprocess/prepare_bm25.py
@@ -2,9 +2,13 @@ from collections import defaultdict
 import json
 import sys
 from pathlib import Path
-inp_path = sys.argv[2]
-out_path = Path(inp_path.replace("splits_psg", "splits_psg_1") + ".jsonl")
+inp_path = sys.argv[1]
 top_k = 1
+out_path = Path(inp_path.replace(
+    "splits_psg", f"splits_psg_{top_k}") + ".jsonl")
+out_dir = out_path.parent
+if not out_dir.exists():
+    out_dir.mkdir()
 doc2psgs = defaultdict(list)
 with open(inp_path, "r") as f:
     for line in f:
@@ -16,5 +20,5 @@ with open(out_path, "w") as f:
     for doc_id in doc2psgs:
         psgs = doc2psgs[doc_id][:top_k]
         text = " ".join(psgs)
-        json_doc = {"id": doc_id, "text": text}
+        json_doc = {"id": doc_id, "contents": text}
         f.write(json.dumps(json_doc)+"\n")
