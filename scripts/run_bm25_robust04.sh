e8e3d28c4f2ef388309911adde07c8f17d75ceac
Thong Nguyen <thongnguyenthac@gmail.com>
2023-04-23 13:11:07 +0200

    run bm25 robust04

---
+++ scripts/run_bm25_robust04.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Exp results" > results_robust04.txt
+num_psgs=(7 8 9 10)
+for n in ${num_psgs[@]}; do 
+mkdir data/trec-robust04/splits_psg_${n}
+for f in data/msmarco_doc/splits_psg/* ; do
+    python lsr/preprocess/prepare_bm25.py $f $n & 
+    pids="$pids $!"
+done
+wait $pids
+
+../anserini-lsr/target/appassembler/bin/IndexCollection -collection JsonCollection -input data/trec-robust04/splits_psg_${n}/ -index indexes/trec-robust04-${n}-bm25 -generator DefaultLuceneDocumentGenerator -threads 60 -storePositions
+
+../anserini-lsr/target/appassembler/bin/SearchCollection -index indexes/trec-robust04-${n}-bm25 -topics data/trec-robust04/desc-queries.tsv -topicreader TsvInt -output runs/trec-robust04_bm25_${n}.trec -parallelism 60 -bm25 -hits 1000 -bm25 -bm25.k1 0.9 -bm25.b 0.4
+
+echo $n >> results_robust04.txt
+ir_measures data/trec-robust04/robust04.qrels runs/trec-robust04_bm25_${n}.trec MRR@10 R@1000 >> results_robust04.txt
+
+unset pids 
+done
