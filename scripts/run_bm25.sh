64daa39475479221951166bc2e41463dc9b28816
Thong Nguyen <thongnguyenthac@gmail.com>
2023-04-23 12:27:27 +0200

    fix retrieval command

--- scripts/run_bm25.sh
+++ scripts/run_bm25.sh
@@ -11,7 +11,7 @@ wait $pids
 
 ../anserini-lsr/target/appassembler/bin/IndexCollection -collection JsonCollection -input data/msmarco_doc/splits_psg_${n}/ -index indexes/msmarco-doc-${n}-bm25 -generator DefaultLuceneDocumentGenerator -threads 60 -storePositions
 
-../anserini-lsr/target/appassembler/bin/SearchCollection -index indexes/msmarco-doc-${n}-bm25  -topics data/msmarco_doc/msmarco-docdev-queries.tsv  -topicreader TsvString -output runs/msmarco_doc_bm25_${n}.trec  -impact -pretokenized -hits 1000 -parallelism 200
+../anserini-lsr/target/appassembler/bin/SearchCollection -index indexes/msmarco-doc-${n}-bm25 -topics data/msmarco_doc/msmarco-docdev-queries.tsv -topicreader TsvInt -output runs/msmarco_doc_bm25_${n}.trec -parallelism 60 -bm25 -hits 1000 -bm25
 
 echo $n >> results.txt
 ir_measures data/msmarco_doc/msmarco-docdev-qrels.tsv runs/msmarco_doc_bm25_${n}.trec MRR@10 R@1000 >> results.txt
