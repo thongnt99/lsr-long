52b4d06ece8a7dbba276948f04f12e63cdd0beda
Thong Nguyen <thongnguyenthac@gmail.com>
2023-04-23 13:39:30 +0200

    need to re-index

--- scripts/run_bm25_robust04.sh
+++ scripts/run_bm25_robust04.sh
@@ -9,7 +9,7 @@ for n in ${num_psgs[@]}; do
 # done
 # wait $pids
 
-# ../anserini-lsr/target/appassembler/bin/IndexCollection -collection JsonCollection -input data/trec-robust04/splits_psg_${n}/ -index indexes/trec-robust04-${n}-bm25 -generator DefaultLuceneDocumentGenerator -threads 60 -storePositions
+../anserini-lsr/target/appassembler/bin/IndexCollection -collection JsonCollection -input data/trec-robust04/splits_psg_${n}/ -index indexes/trec-robust04-${n}-bm25 -generator DefaultLuceneDocumentGenerator -threads 60 -storePositions -storePositions -storeDocvectors -storeRaw
 
 ../anserini-lsr/target/appassembler/bin/SearchCollection -index indexes/trec-robust04-${n}-bm25 -topics data/trec-robust04/desc-queries.tsv -topicreader TsvInt -output runs/trec-robust04_bm25_${n}.trec -parallelism 60 -bm25 -hits 1000 -bm25 -bm25.k1 0.9 -bm25.b 0.4 -rm3 -rm3.fbTerms 10 -rm3.fbDocs 10 -rm3.originalQueryWeight 0.5
 
