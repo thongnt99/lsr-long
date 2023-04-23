dbf7be5fbec8a2ef3df3afa0aecc1b3aee2f35fb
Thong Nguyen <thongnguyenthac@gmail.com>
2023-04-24 00:38:46 +0200

    fix bugs

--- scripts/long_doc_max_score.sh
+++ scripts/long_doc_max_score.sh
@@ -5,6 +5,6 @@ echo "Exp results" > $output_file
 num_psgs=(1 2 3 4 5 6 7 10)
 for n in ${num_psgs[@]}; do 
 echo $n >> $output_file
-python lsr/long_documents/max_score_aggregation.py data/msmarco_doc/run_psgs.trec data/msmarco_doc/run_max_score_${n} $n 
-ir_measures data/msmarco_doc/msmarco-docdev-qrels.tsv ${input_dir}/run_${aggr}_${n}.trec MRR@10 NDCG@10 R@1000 >> $output_file 
+python lsr/long_documents/max_score_aggregation.py ${input_dir}/run_psgs.trec ${input_dir}/run_max_score_${n}.trec $n 
+ir_measures data/msmarco_doc/msmarco-docdev-qrels.tsv ${input_dir}/run_max_score_${n}.trec MRR@10 NDCG@10 R@1000 >> $output_file 
 done
