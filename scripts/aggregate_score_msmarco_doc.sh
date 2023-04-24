42a34a67531e2aff32c394a2d5591decd0562103
Thong Nguyen <thongnguyenthac@gmail.com>
2023-04-24 17:37:56 +0200

    aggregate score

--- scripts/aggregate_score_msmarco_doc.sh
+++ scripts/aggregate_score_msmarco_doc.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 input_dir=data/msmarco_doc
 output_file=${input_dir}/results_max_score.txt
+echo "Indexing passages"
+
+../anserini-lsr/target/appassembler/bin/IndexCollection -collection JsonSparseVectorCollection -input ${input_dir}/doc_vectors -index ${input_dir}/index_psgs  -generator SparseVectorDocumentGenerator -threads 200 -impact -pretokenized
+
+echo "Retrieval top relevant passages"
+
+../anserini-lsr/target/appassembler/bin/SearchCollection -index ${input_dir}/index_psgs  -topics ${input_dir}/query.tsv -topicreader TsvString -output ${input_dir}/run_psgs.trec  -impact -pretokenized -hits 10000 -parallelism 200
+
+echo "Aggregating and evaluting"
+
 echo "Exp results" > $output_file
 num_psgs=(1 2 3 4 5 6 7 10)
 for n in ${num_psgs[@]}; do 
