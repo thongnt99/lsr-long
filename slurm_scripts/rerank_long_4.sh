fc56ac310617eee3bbf65aacd703924ca9dfaf01
Thong Nguyen <thongnguyenthac@gmail.com>
2023-04-24 07:42:51 +0200

    make conda available

--- slurm_scripts/rerank_long_4.sh
+++ slurm_scripts/rerank_long_4.sh
@@ -7,5 +7,8 @@
 #SBATCH -t 0-03:00:00
 #SBATCH -o slurm_scripts/logs/rerank_long_4.out
 #SBATCH -e slurm_scripts/logs/rerank_long_4.err
+# Make conda available:
+eval "$(conda shell.bash hook)"
+# Activate a conda environment:
 conda activate lsr 
 python -m lsr.rerank_long -run data/msmarco_doc/run_max_score_4.trec -q data/msmarco_doc/msmarco-docdev-queries.tsv -d data/msmarco_doc/collection_psgs.tsv -npsg 4 -cp outputs/reranker_exact_qmlp_dmlm_msmarco_doc_ce_4_psg/checkpoint-20000
