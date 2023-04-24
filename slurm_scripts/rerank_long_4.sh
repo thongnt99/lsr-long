1ecfcbcafc161c8fa7e57be30fcf6a43da03175b
Thong Nguyen <thongnguyenthac@gmail.com>
2023-04-24 07:40:17 +0200

    reranking

---
+++ slurm_scripts/rerank_long_4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --nodes 1
+#SBATCH -p gpu20
+#SBATCH --gres gpu:1
+#SBATCH -c 5
+#SBATCH --mem-per-cpu 15000
+#SBATCH -t 0-03:00:00
+#SBATCH -o slurm_scripts/logs/rerank_long_4.out
+#SBATCH -e slurm_scripts/logs/rerank_long_4.err
+conda activate lsr 
+python -m lsr.rerank_long -run data/msmarco_doc/run_max_score_4.trec -q data/msmarco_doc/msmarco-docdev-queries.tsv -d data/msmarco_doc/collection_psgs.tsv -npsg 4 -cp outputs/reranker_exact_qmlp_dmlm_msmarco_doc_ce_4_psg/checkpoint-20000
