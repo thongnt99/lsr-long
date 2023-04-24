2cb8195aa083de836ea0c9d12d136d3961f53a46
Thong Nguyen <thongnguyenthac@gmail.com>
2023-04-24 08:30:00 +0200

    reduce batch size

--- slurm_scripts/rerank_long_4.sh
+++ slurm_scripts/rerank_long_4.sh
@@ -11,4 +11,4 @@
 eval "$(conda shell.bash hook)"
 # Activate a conda environment:
 conda activate lsr 
-python -m lsr.rerank_long -run data/msmarco_doc/run_max_score_4.trec -q data/msmarco_doc/msmarco-docdev-queries.tsv -d data/msmarco_doc/collection_psgs.tsv -npsg 4 -cp outputs/reranker_exact_qmlp_dmlm_msmarco_doc_ce_4_psg/checkpoint-20000 -qrel data/msmarco_doc/msmarco-docdev-qrels.tsv
+python -m lsr.rerank_long -run data/msmarco_doc/run_max_score_4.trec -q data/msmarco_doc/msmarco-docdev-queries.tsv -d data/msmarco_doc/collection_psgs.tsv -npsg 4 -cp outputs/reranker_exact_qmlp_dmlm_msmarco_doc_ce_4_psg/checkpoint-20000 -qrel data/msmarco_doc/msmarco-docdev-qrels.tsv -bs 32
